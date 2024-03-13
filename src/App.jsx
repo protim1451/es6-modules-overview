7f0af909489b5be7d9f19bbb3f505eaa2c6d75a1
protim1451 <partha.doict@gmail.com>
2024-03-13 23:42:51 +0600

    41-3 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
-import { useState } from 'react'
-
+import { useEffect, useState } from 'react'
+import Sunglass from './components/Sunglass/Sunglass'
 import './App.css'
+import Watch from './components/watch/Watch'
 
 function App() {
   const [count, setCount] = useState(0)
+  // const watches = [
+  //   {id: 1, name: 'Apple watch', price: 200},
+  //   {id: 2, name: 'Samsung watch', price: 150},
+  //   {id: 3, name: 'MI watch', price: 250},
+  // ]
+    const [watches, setWatches] = useState([]);
 
+
+    useEffect(() =>{
+      fetch('watches.json')
+      .then(res => res.json())
+      .then(data => setWatches(data));
+    }, [])
+
+  
   return (
     <>
       
       <h1>ES6 Modules Overview</h1>
-      
+      {
+        watches.map(watch => 
+        <Watch key={watch.id} watch={watch}></Watch>)
+      }
     </>
   )
 }
--- src/components/watch/Watch.jsx
+++ src/components/watch/Watch.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Watch.css'; 
 
-const Watch = () => {
+const Watch = ({watch}) => {
+    const {name, price} = watch;
     return (
         <div>
-            
+            <h2>Watches: {name}</h2>
+            <p>Price: {price}</p>
         </div>
     );
 };
